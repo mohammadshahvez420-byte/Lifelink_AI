7284debca5afafb41bfda806b5dc5957fe2d0507
Mohammad Shahvez <mohammadshahvez420@gmail.com>
2026-08-01 22:56:12 +0530

    Day 8: Added Blood Donor Registration API

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,4 +10,35 @@ app = FastAPI(
 def home():
     return {
         "message": "Welcome to LifeLink AI 🚑"
+    }  
+
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="LifeLink AI",
+    description="AI-Powered Blood Donor Search System",
+    version="1.0.0"
+)
+
+class Donor(BaseModel):
+    name: str
+    age: int
+    blood_group: str
+    city: str
+    phone: str
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to LifeLink AI 🚑"
+    }
+
+@app.post("/donors/register")
+def register_donor(donor: Donor):
+    return {
+        "message": "Donor registered successfully",
+        "donor": donor
     }
