ce0c9ea8c53aa83aececcbbf9aa9bae7b270f2dd
Mohammad Shahvez <mohammadshahvez420@gmail.com>
2026-08-08 00:08:56 +0530

    Day 9: Moved Donor model to models folder

--- backend/app/routes/donor.py
+++ backend/app/routes/donor.py
@@ -1,31 +1,18 @@
 from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/donors")
-def get_donors():
-    return {
-        "message": "List of donors will appear here"
-    }
-
-
-
-
-
-
-from fastapi import APIRouter
 from app.models.donor import Donor
 
 router = APIRouter()
 
+# Temporary donor storage
+donors = []
+
 @router.get("/")
 def get_donors():
-    return {
-        "message": "List of donors will appear here"
-    }
+    return donors
 
 @router.post("/register")
 def register_donor(donor: Donor):
+    donors.append(donor)
     return {
         "message": "Donor registered successfully",
         "donor": donor
